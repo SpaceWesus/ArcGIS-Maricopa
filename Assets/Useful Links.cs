2e2fad0ccc368dc32c0d69ef03a606455fe1fe83
LF Computer <talon172jr@gmail.com>
2023-12-06 13:30:43 -0700

    Update Useful Links.cs

--- Assets/Useful Links.cs
+++ Assets/Useful Links.cs
@@ -1,4 +1,4 @@
 // Both options for inputting 3D Models from google earth
-// https://www.reddit.com/r/virtualreality/comments/xk6iof/you_can_download_any_3d_models_from_google_map/
+// https://www.youtube.com/watch?v=FiLN_d1bGRU
 // https://www.youtube.com/watch?v=dSWL5cougVQ
 
